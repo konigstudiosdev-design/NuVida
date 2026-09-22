87ab9bf8b9565a808e2d62dc51032df8377abdda
Eder Diaz <eder.uxui@gmail.com>
2026-09-21 21:58:00 -0600

    chore: remove source code, keep only release binaries

--- electron/preload.cjs
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron')
-
-// Expose protected Electron Native API bridge to renderer
-contextBridge.exposeInMainWorld('electronAPI', {
-  platform: process.platform,
-  isElectron: true,
-  sendNotification: (title, body) => ipcRenderer.send('notify', { title, body }),
-})
